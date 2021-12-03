92f4c53385f804919d14bdea313df3ae529bc7f1
marcello <marcellopettenuzzo2@gmail.com>
2021-12-03 16:42:53 +0100

    3 script

--- js/function.js
+++ js/function.js
@@ -1,12 +1,18 @@
 /* call dom */
 const userTicket = document.getElementById("yourTicket");
 const userData = document.getElementById("userData");
+const sale = document.getElementById("sale");
 
 /* variable */
 const tripKm = prompt('Inserire il numero di Km');
 const userName = prompt('Il tuo nome');
 const userAge = prompt('Inserire la propria età');
 const verifyAge = parseInt(userAge);
+let userNameOutput =
+`
+Salve  ${userName}, ecco il prezzo del tuo biglietto per un totale di ${tripKm} Km:
+` 
+;
 let currency = "€";
 console.log(verifyAge + "anni");
 
@@ -18,18 +24,20 @@ console.log("Prezzo intero" + " " + triPrice + currency);
 if (verifyAge <= 18) {
     let undergradeSale = (triPrice * 20) / 100;
     let priceUndergrade = triPrice - undergradeSale;
-    userData.innerHTML = "Salve " + userName + " ecco il tuo biglietto:";
-    userTicket.innerHTML = "+ 20% di sconto" + " " + priceUndergrade.toFixed(2) + currency;
+    userData.innerHTML = userNameOutput;
+    userTicket.innerHTML = priceUndergrade.toFixed(2) + currency;
+    sale.innerHTML = "20% di sconto";
 }
 
 else if (verifyAge >= 65) {
     let overSale = (triPrice * 40) / 100;
     let priceOver = triPrice - overSale;
-    userData.innerHTML = "Salve " + userName + " ecco il tuo biglietto:";
-    userTicket.innerHTML = "+ 40% di sconto" + " " + priceOver.toFixed(2) + currency;    
+    userData.innerHTML = userNameOutput;
+    userTicket.innerHTML = priceOver.toFixed(2) + currency;   
+    sale.innerHTML = "40% di sconto"; 
 }
 
 else {
-    userData.innerHTML = "Salve " + userName + " ecco il tuo biglietto:";
+    userData.innerHTML = userNameOutput;
     userTicket.innerHTML = triPrice.toFixed(2) + currency;
 }
