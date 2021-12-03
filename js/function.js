c3a05599966054923f4a834ed8948620781b170c
marcello <marcellopettenuzzo2@gmail.com>
2021-12-03 17:16:35 +0100

    5 script

--- js/function.js
+++ js/function.js
@@ -8,12 +8,16 @@ const tripKm = prompt('Inserire il numero di Km');
 const userName = prompt('Il tuo nome');
 const userAge = prompt('Inserire la propria età');
 const verifyAge = parseInt(userAge);
+/* variable + infoText*/
 let userNameOutput =
 `
 Salve  ${userName}, ecco il prezzo del tuo biglietto per un totale di ${tripKm} Km:
-` 
+`
 ;
+let user18 = "<span> 20% di sconto</span>";
+let user65 = "<span> 40% di sconto</span>"
 let currency = "€";
+
 console.log(verifyAge + "anni");
 
 /* km price */
@@ -27,15 +31,13 @@ userData.innerHTML = userNameOutput;
 if (verifyAge <= 18) {
     let undergradeSale = (triPrice * 20) / 100;
     let priceUndergrade = triPrice - undergradeSale;
-    userTicket.innerHTML = priceUndergrade.toFixed(2) + currency;
-    sale.innerHTML = "20% di sconto";
+    userTicket.innerHTML = priceUndergrade.toFixed(2) + currency + user18;
 }
 
 else if (verifyAge >= 65) {
     let overSale = (triPrice * 40) / 100;
     let priceOver = triPrice - overSale;
-    userTicket.innerHTML = priceOver.toFixed(2) + currency;   
-    sale.innerHTML = "40% di sconto"; 
+    userTicket.innerHTML = priceOver.toFixed(2) + currency + user65; 
 }
 
 else {
